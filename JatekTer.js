b0b7ed14f47eca31820aad30f600f17b218c24e6
Nao <ktimeabarbara@gmail.com>
2025-03-26 10:56:25 +0100

    js fileok alap

---
+++ JatekTer.js
@@ -0,0 +1,23 @@
+import Lampa from "./Lampa.js";
+
+export default class JatekTer {
+    #lampaLista = [" "," "," "," "," "," "," "," "," "]
+    constructor(szuloElem){
+        this.szuloElem=szuloElem;
+        this.megjelenit();
+        this.esemenyKezelo();
+    }
+    esemenyKezelo(){
+        window.addEventListener("kivalaszt",(event)=>{
+            let index=event.detail;
+            this.#lampaLista[index]="*";
+            this.szuloElem.innerHTML="";
+            this.megjelenit();
+        })
+    }
+    megjelenit(){
+        for (let index = 0; index < this.#lampaLista.length; index++) {
+            const LAMPA = new Lampa(this.#lampaLista[index],index,this.szuloElem)
+        }
+    }
+}
---
+++ Lampa.js
@@ -0,0 +1,28 @@
+
+
+export default class Lampa {
+    #felkapcs
+    #index
+    constructor(felkapcs, index, szuloElem){
+        this.#felkapcs=felkapcs;
+        this.#index=index;
+        this.szuloElem=szuloElem;
+        this.megjelenit();
+        this.esemenyKezelo();
+    }
+    megjelenit(){
+        let html = `<div class="lampaElem">
+                        <div>${this.#felkapcs}</div>
+                    </div>`;
+        this.szuloElem.insertAdjacentHTML("beforeend",html);
+    }
+    esemenyKezelo(){
+        this.elem=document.querySelector(".elem:last-child")
+        this.elem.addEventListener("click",(event)=>{
+            console.log(this.#index)
+            const e = new CustomEvent("kivalaszt",{detail:this.#index})
+            window.dispatchEvent(e);
+        })
+    }
+    
+}
